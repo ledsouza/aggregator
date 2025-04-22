50c927439a7fbc6b568513271a1b1d5284221bc5
ledsouza <leandro.souza.159@gmail.com>
2025-04-22 19:32:24 -0300

    added add feed cmd

--- handler_feed.go
+++ handler_feed.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/ledsouza/aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -10,6 +14,35 @@ func handlerAgg(s *state, cmd command) error {
 	feed, err := fetchFeed(context.Background(), url)
 	if err != nil {
 		return err
+	}
+
+	fmt.Printf("%+v\n", feed)
+	return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.Args) < 2 {
+		return fmt.Errorf("name and url are required")
+	}
+
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+
+	user, err := s.db.GetUser(context.Background(), s.config.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("couldn't get current user: %w", err)
+	}
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create the feed: %w", err)
 	}
 
 	fmt.Printf("%+v\n", feed)
